d9d7bf6faf2162aad63c54e23606c3b823e6528e
Özcan Kara <ozcankara810@gmail.com>
2023-06-03 18:00:55 +0300

    some changes

--- 06-V-If-V-Show/app.js
+++ 06-V-If-V-Show/app.js
@@ -1,15 +1,22 @@
 const app = Vue.createApp({
    
 template:` 
-    <div></div>
+    <div v-if='showInfo'>Deneme Yazısı</div>
+    <div v-show='!showInfo'>Deneme Yazısı -2 </div>
+      <button @click="toggle">
+      <span v-if="!showInfo">Göster</span>
+      <span v-else>Gizle</span>
+     </button>
      `,
      data(){
         return{
-           
+           showInfo:true
         }
      },
      methods: {
-        
+      toggle(){
+         this.showInfo=!this.showInfo;
+      }
      },
 }); 
 app.mount('#app') 
