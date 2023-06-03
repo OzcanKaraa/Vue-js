7e8d48da69bf6ef2e92ad2ca1abda2da60b9d709
Özcan Kara <ozcankara810@gmail.com>
2023-06-03 12:56:41 +0300

    some changes

---
+++ 02-Vue-Uygulama/app.js
@@ -0,0 +1,5 @@
+const app = Vue.createApp({
+   template:`<h3>Vue ilk Uygulama</h3>`
+}); 
+
+app.mount('.app') 
---
+++ 03-Vue-Template/app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    // template:`<h2>VueJs Template - Js</h2>`
+template:` 
+    <h2>Vue Js </h2>
+    <b>Javascript</b>
+    <p>Framework</p> 
+     `
+}); 
+app.mount('#app') 
